0512d2d265e8876d5b2c8044784531f8f14e22d9
Cvolton <cvolton@cvolton.eu>
2026-07-14 20:27:31 +0200

    use sharex 8.1 build because it still supports gtx 10 series

--- installer/prepare-deps.py
+++ installer/prepare-deps.py
@@ -5,22 +5,22 @@ import os
 # FFmpeg
 print("Downloading FFmpeg essentials build...")
 
-response = requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z")
+response = requests.get("https://github.com/ShareX/FFmpeg/releases/download/v8.1/ffmpeg-8.1-win-x64.zip")
 response.raise_for_status()
 
 print("Extracting FFmpeg essentials build...")
 
-with open("ffmpeg-release-essentials.7z", "wb") as f:
+with open("ffmpeg-8.1-win-x64.zip", "wb") as f:
     f.write(response.content)
 
 os.makedirs("files/ffmpeg", exist_ok=True)
 
 subprocess.run(
-    ["7z", "e", "ffmpeg-release-essentials.7z", "-ofiles/ffmpeg", "ffmpeg.exe", "-r", "-y"],
+    ["7z", "e", "ffmpeg-8.1-win-x64.zip", "-ofiles/ffmpeg", "ffmpeg.exe", "-r", "-y"],
     check=True,
 )
 
-os.remove("ffmpeg-release-essentials.7z")
+os.remove("ffmpeg-8.1-win-x64.zip")
 
 print("FFmpeg essentials build downloaded and extracted successfully.")
 
